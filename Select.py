cbe3e7cea8daf04e8e6e2926d17a5d01bb96138d
Alex Tuchak <alextuchak@gmail.com>
2021-07-24 15:42:24 +0500

    task5 v2

--- Select.py
+++ Select.py
@@ -40,10 +40,42 @@ pprint(connection.execute(""" SELECT DISTINCT m.name FROM artists a
 
 # 6.название альбомов, в которых присутствуют исполнители более 1 жанра;
 
-pprint(connection.execute(""" SELECT name FROM albums
-WHERE id = (
-SELECT artistalbum FROM artists
- GROUP BY artistalbum
-HAVING COUNT(name) > 1
-);""").fetchall())
+pprint(connection.execute(""" SELECT al.name FROM albums al
+LEFT JOIN artists ar ON al.id = ar.artistalbum
+WHERE ar.name = (
+SELECT name FROM artists
+GROUP by name
+HAVING MAX(artistgenre) != AVG(artistgenre)) """).fetchall())
+# В моей БД связь артиста с жанрами происходит через столбец INTEGER artistgenre. Если артист поет в одном жанре, то
+# при любом кол-ве записей с разными альбомами AVG значение столбца artistgenre будет равно самому себе. Если поет в
+# разных условие запроса выполнится
 
+# 7. наименование треков, которые не входят в сборники;
+pprint(connection.execute(""" SELECT DISTINCT t.name FROM tracks t
+LEFT JOIN mixtapes m ON t.trackmixtapes = m.id
+WHERE t.trackmixtapes IS NULL;
+""").fetchall())
+
+# 8.исполнителя(-ей), написавшего самый короткий по продолжительности трек (теоретически таких треков может быть
+# несколько);
+
+pprint(connection.execute(""" SELECT name, length from tracks
+WHERE length = (
+SELECT MIN(length) from tracks);
+""").fetchall())
+
+# 9. название альбомов, содержащих наименьшее количество треков.
+pprint(connection.execute("""
+CREATE TABLE IF NOT EXISTS temptable
+(Name VARCHAR(64), albumid INTEGER);
+INSERT INTO temptable SELECT DISTINCT name, trackalbum from tracks;
+
+SELECT al.name FROM albums al
+LEFT JOIN temptable te ON te.albumid = al.id
+GROUP BY al.name
+HAVING COUNT(al.name) <= 1;
+
+ """).fetchall())
+(connection.execute("""DROP TABLE temptable """).fetchall())
+ # В моей БД есть песня которую исполняют два исполнителя и это единственная песня в альбоме. Для корректного вывода
+ # названия альбомов с наименьшим колличеством песен использовал промежуточную фильтрацию через временную таблицу
